690d97fc3ba6ae62ec3c9b7fdb6ab009e28d56c0
sailxjx <sailxjx@163.com>
2013-06-14 12:14:24 +0800

    add colorfull

--- colorfull/README.md
+++ colorfull/README.md
@@ -5,3 +5,9 @@ Colorfull
 A fullscreen color picker, inspired by the guy named Jony Ive.
 
 [Some Jokes](http://jonyiveredesignsthings.tumblr.com/)
+
+## TODO
+
+* add color picker
+* self chosen colors
+* other animations
--- colorfull/js/index.js
+++ colorfull/js/index.js
@@ -1,36 +1,83 @@
 (function($) {
 
-  var $canvas = $('#colorfull-canvas');
-
   $.fn.colorfull = (function($) {
+
     function colorfull() {
+
       if (this.prop('tagName') !== 'CANVAS') {
         throw this.selector + " is not a canvas";
       }
-      var $canvas = this,
-          $window = $(window),
-          ctx = $canvas[0].getContext('2d');
 
-      this.height($window.height()).width($window.width());
+      var $window = $(window),
+          $canvas = this,
+          domCanvas = $canvas[0],
+          ctx,
+          animateTiming,
+          duration = 5;  // seconds for every loop
+          lastDegree = 0;
 
-      ctx.fillstyle="#333";
-      ctx.fillRect(0, 0, $canvas.width(), $canvas.height());
+      var resize = function() {
+        // it will not work if only change the css height/width of canvas
+        domCanvas.height = $window.height();
+        domCanvas.width = $window.width();
+      };
+
+      var pos = function() {
+        var width = $canvas.width(),
+            height = $canvas.height(),
+            x1, y1, x2, y2, frames = duration * 1000 / 30;
+        lastDegree = typeof lastDegree === 'undefined' ? 0 : (lastDegree + Math.PI * 2 / frames) % (Math.PI * 2);
+        console.log(lastDegree);
+        x1 = width * (1 + Math.cos(lastDegree)) / 2;
+        y1 = height * (1 + Math.sin(lastDegree)) / 2;
+        x2 = width * (1 + Math.cos(lastDegree + Math.PI)) / 2;
+        y2 = height * (1 + Math.sin(lastDegree + Math.PI)) / 2;
+
+        console.log([x1, y1, x2, y2]);
+
+        return [x1, y1, x2, y2];
+      };
+
+      var draw = function() {
+        clean();
+        coordinates = pos();
+        var grd = ctx.createLinearGradient(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+
+        grd.addColorStop(0,"#F800FF");
+        grd.addColorStop(1,"#32FFC6");
+
+        ctx.fillStyle=grd;
+        ctx.fillRect(0, 0, $canvas.width(), $canvas.height());
+
+      };
+
+      var clean = function() {
+        ctx.clearRect(0, 0, $canvas.width(), $canvas.height());
+      };
+
+      var init = function() {
+        if (animateTiming) {
+          clearInterval(animateTiming);
+        }
+        resize();
+        ctx = $canvas[0].getContext('2d');
+        draw();
+        animateTiming = setInterval(draw, 30);
+        return animateTiming;
+      };
+
+      init();
+
+      $window.bind('resize', function(e) {
+        init();
+      });
+
     }
     return colorfull;
   })(jQuery);
 
+  var $canvas = $('#cf-canvas');
   $canvas.colorfull();
-
-  // draw = function() {
-  //   $canvas.height($window.height());
-  //   ctx.fillStyle="#333";
-  //   ctx.fillRect(0, 0, $canvas.width(), $canvas.height());
-  // };
-
-  // draw();
-
-  // $(window).bind('resize', function(e) {
-  //   draw();
-  // });
+  $('#copyleft').fadeOut(10000);
 
 })(jQuery);
